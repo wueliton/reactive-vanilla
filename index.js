6723d802e079c18cdd82a01fa287755e12389939
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2026-09-10 21:38:26 -0300

    fix: removed dynamic styles and added static styles

--- index.js
+++ index.js
@@ -1,23 +1,21 @@
 import { router } from "./core/router/index.js";
 
-import { } from "./twid.js";
-
 const routes = router({
-    outlet: '.router-outlet',
+    outlet: ".router-outlet",
     routes: {
-        '/': {
-            html: './index.html',
-            script: () => import('./home.js')
+        "/": {
+            html: "./index.html",
+            script: () => import("./home.js"),
         },
-        '/examples': {
-            html: './examples/index.html',
-            script: () => import('./examples/index.js')
+        "/examples": {
+            html: "./examples/index.html",
+            script: () => import("./examples/index.js"),
         },
-        '/examples/otp': {
-            html: './examples/otp/index.html',
-            script: () => import('./examples/otp/index.js')
+        "/examples/otp": {
+            html: "./examples/otp/index.html",
+            script: () => import("./examples/otp/index.js"),
         },
-    }
+    },
 });
 
 routes.start();
