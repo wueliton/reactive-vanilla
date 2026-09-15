6645c51b22d9e2aa4e0b985f91c2460715effea1
Paulo Wuéliton <paulo.wueliton@hotmail.com>
2026-09-14 21:16:52 -0300

    feat: updated otp imports and updated styles

--- examples/otp/index.js
+++ examples/otp/index.js
@@ -1,6 +1,5 @@
-import { createCodeEditor, createOtp } from "../../components/index.js";
+import { createCodeEditor } from "../../components/index.js";
 import { component } from "../../dist/reactive.js";
-import { hightlight } from "../../utils/highlight.js";
 
 export function page(root) {
   component("[code-editor]", createCodeEditor());
